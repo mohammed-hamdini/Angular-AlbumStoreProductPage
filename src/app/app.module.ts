884dfc2ab26970ec39bec3764b31b98e489adf38
mohammed-hamdini <78077047+mohammed-hamdini@users.noreply.github.com>
2023-02-16 17:12:30 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { HttpModule } from '@angular/http';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
+    ProductListComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [ProductService],
